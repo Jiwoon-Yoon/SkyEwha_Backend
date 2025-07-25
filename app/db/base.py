020c8407797a7444679125a89bb2708313e2003a
Hwakyong Yoo <129262146+hwakyong-Yoo@users.noreply.github.com>
2025-07-25 12:11:50 +0900

    fix/feature 마이그레이션 파일 업로드 , base.py 수정

--- app/db/base.py
+++ app/db/base.py
@@ -1,10 +1,4 @@
 # app/db/base.py
-from sqlalchemy.ext.declarative import  declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-# 여기에 모든 모델 import
-from app.models.user import User
-from app.models.video import Video
-from app.models.keyword import Keyword
-from app.models.youtube import YouTubeVideo
