f1b494c7edee6dbf3409dfd2ab58e45f18a05ff4
Jiwoon Yoon <wldns0502@gmail.com>
2025-07-26 14:35:38 +0900

    feature/feedback: 마이그레이션 오류 수정

--- app/models/keyword.py
+++ app/models/keyword.py
@@ -7,11 +7,11 @@ class Keyword(Base):
     __tablename__ = "keywords"
 
     keyword_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    video_id = Column(Integer, ForeignKey("videos.video_id", ondelete="CASCADE"), nullable=False)
+    video_id = Column(Integer, ForeignKey("videos.video_id", ondelete="CASCADE"), nullable=False) #FK
     keyword = Column(String, nullable=False)
     embedding = Column(Vector(384), nullable=True)  # 예: 384차원 벡터, 모델에 따라 조정 가능
 
-    video = relationship("Video", back_populates="keywords")
+    video = relationship("Video", back_populates="keywords", passive_deletes=True)
 
     def __repr__(self):
         return f"<Keyword(id={self.keyword_id}, video_id={self.video_id}, keyword='{self.keyword}')>"
--- migration/env.py
+++ migration/env.py
@@ -2,22 +2,18 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+
 from alembic import context
 
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
 from app.core.config import settings
+from app.db.base import Base
+from pgvector.sqlalchemy import Vector
+import app.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
-
-# Logging 설정
-fileConfig(config.config_file_name)
-
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -28,8 +24,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.db.base import Base
-from pgvector.sqlalchemy import Vector
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
